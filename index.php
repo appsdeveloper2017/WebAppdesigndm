332bc39d88a460ed797b199b47a33986137a59a3
david <dav_mm@hotmail.com>
2017-12-19 00:12:52 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+Created by PhpStorm.
+User: david
+Date: 18/12/17
+Time: 23:24
+
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Hooooooooolaaaaaaaaaaa!!</h1>
+        <?php
+                echo "Hooooooooolaaaaaaaaaaa";
+        // put your code here
+        ?>
+    </body>
+</html>
