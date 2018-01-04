91a919390deb2854ba5ba33ada67f85f54f15037
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-01-04 22:35:45 +0100

    Prueba responsiva del proyecto.

--- appdesigndm.js
+++
@@ -1,9 +0,0 @@
-function mostraralerta() {
-    alert('¡Hizo click!');
-}
-
-function hacerclick() {
-    document.getElementsByTagName('li')[0].onclick = mostraralerta;
-}
-
-window.onload = hacerclick;
