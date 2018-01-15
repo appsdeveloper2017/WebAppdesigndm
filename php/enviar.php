dccf56d03b57e77d0ccc06bc23ee9fc68ef451ac
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-01-15 22:09:37 +0100

    Se añade el formulario de contacto y se añade el archivo php funcionando, falta añadir pantalla de confirmación de envío de los datos.

---
+++ php/enviar.php
@@ -0,0 +1,12 @@
+﻿<?php
+   $destino= "alexmailwork@gmail.com";
+   $nombre = $_POST["nombre"];
+   $apellidos = $_POST["apellidos"];
+   $correo = $_POST["correo"];
+   $telefono = $_POST["telefono"];
+   $asunto = $_POST["asunto"];
+   $mensaje = $_POST["mensaje"];
+   $contenido = "Nomb: " . $nombre . "\nCognoms: " . $apellidos . "\nCorreu: " . $correo . "\nTeléfon: " . $telefono . "\nTema: " . $asunto . "\nMissatge: " . $mensaje;
+   mail($destino,"Portal documentació tècnica", $contenido);
+   header("location:index.html");
+?>
