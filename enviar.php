76ad65468b76608123c96f89e0a9ae9c38893360
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-01-29 22:33:52 +0100

    Se implementa la captcha, funciona correctamente y es reponsive.

--- enviar.php
+++ enviar.php
@@ -1,12 +1,36 @@
 ﻿<?php
-   $destino= "alexmailwork@gmail.com";
-   $nombre = $_POST["nombre"];
-   $apellidos = $_POST["apellidos"];
-   $correo = $_POST["correo"];
-   $telefono = $_POST["telefono"];
-   $asunto = $_POST["asunto"];
-   $mensaje = $_POST["mensaje"];
-   $contenido = "Nomb: " . $nombre . "\nCognoms: " . $apellidos . "\nCorreu: " . $correo . "\nTeléfon: " . $telefono . "\nTema: " . $asunto . "\nMissatge: " . $mensaje;
-   mail($destino,"Webappdesigndm", $contenido);
-   header("location:index.html");
+
+   $recaptcha = $_POST['g-recaptcha-response'];
+   if ($recaptcha!=''){
+
+        $secret = "6LeF_kIUAAAAANIIusx1wqtDEDJYrZXYs48WsFNT";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $var = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$recaptcha&remoteip=$ip");
+        $array = json_decode($var, true);
+
+        if($array['success']){
+
+            $destino= "appscodedeveloper2017@gmail.com";
+            $nombre = $_POST["nombre"];
+            $apellidos = $_POST["apellidos"];
+            $correo = $_POST["correo"];
+            $telefono = $_POST["telefono"];
+            $asunto = $_POST["asunto"];
+            $mensaje = $_POST["mensaje"];
+            $contenido = "Nomb: " . $nombre . "\nCognoms: " . $apellidos . "\nCorreu: " . $correo . "\nTeléfon: " . $telefono . "\nTema: " . $asunto . "\nMissatge: " . $mensaje;
+            mail($destino,"Mensaje de appdesigndm", $contenido);
+            header("location:index.html");
+
+        }else{
+            "¿Eres un robot?";
+        }
+
+   }else{
+
+        echo "Valide el captcha...";
+
+   }
+
+
+
 ?>
